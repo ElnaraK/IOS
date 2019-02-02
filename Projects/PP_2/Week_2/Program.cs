34ba68e61f5e9f8e9fc275702b9c925b13596ebb
Elnara <elnarak@MacBook-Pro-Elnara.local>
2019-02-02 21:41:41 +0600

    PP2

---
+++ Projects/PP_2/Week_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    class MainClass
+    {
+        private static void Main (string [] args)
+        {
+            using (FileStream fs = new FileStream(@"/Users/elnarak/Desktop/PP2/pal.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string text = sr.ReadLine();
+                    text.ToCharArray();
+
+                    for(int i=0; i<=(text.Length/2); ++i)
+                    {
+                        if (text[i] != text[text.Length - 1-i])
+                        {
+                            Console.WriteLine("No");
+                            break;
+                        }
+                        else if (i == text.Length / 2)
+                        {
+                            Console.WriteLine("Yes");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
